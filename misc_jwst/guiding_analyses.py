34c68009a8ba8165a9fab3f31ffbc1131fe1e12a
Marshall Perrin <mperrin@stsci.edu>
2023-05-30 17:46:59 -0400

    add guiding_performance_jitterball

--- misc_jwst/guiding_analyses.py
+++ misc_jwst/guiding_analyses.py
@@ -13,14 +13,14 @@ import matplotlib.pyplot as plt
 
 def mast_retrieve_guiding_files(filenames, out_dir='.'):
     """Download one or more guiding data products from MAST
-    
-    If the file is already present in the specified local directory, it's not downloaded again. 
-    
+
+    If the file is already present in the specified local directory, it's not downloaded again.
+
     """
-    
+
     mast_url='https://mast.stsci.edu/api/v0.1/Download/file'
     uri_prefix = 'mast:JWST/product/'
-    
+
     outputs = []
 
     mast_api_token = os.environ.get('MAST_API_TOKEN')
@@ -28,7 +28,7 @@ def mast_retrieve_guiding_files(filenames, out_dir='.'):
         headers=dict(Authorization=f"token {mast_api_token}")
     else:
         headers=None
-    
+
     for p in filenames:
         outfile = os.path.join(out_dir, p)
 
@@ -61,20 +61,20 @@ def set_params(parameters):
 
 @functools.lru_cache
 def find_relevant_guiding_file(sci_filename, verbose=True):
-    """ Given a filename of a JWST science file, retrieve the relevant guiding data product. 
+    """ Given a filename of a JWST science file, retrieve the relevant guiding data product.
     This uses FITS keywords in the science header to determine the time period and guide mode,
     and then retrieves the file from MAST
-    
+
     """
-    
-    
+
+
     sci_hdul = fits.open(sci_filename)
-    
+
     progid = sci_hdul[0].header['PROGRAM']
     obs = sci_hdul[0].header['OBSERVTN']
     guidemode = sci_hdul[0].header['PCS_MODE']
-    
-    
+
+
     # Set up the query
     keywords = {
     'program': [progid]
@@ -105,7 +105,7 @@ def find_relevant_guiding_file(sci_filename, verbose=True):
         #products = list(set([x.replace('_cal','_uncal') for x in fn]))
     products.sort()
 
-        
+
     if verbose:
         print(f"For science data file: {sci_filename}")
         print("Found guiding telemetry files:")
@@ -128,16 +128,16 @@ def find_relevant_guiding_file(sci_filename, verbose=True):
     print(delta_times)
     delta_times[delta_times<0] = np.nan
     print(delta_times)
-    
+
     wmatch = np.argmin(np.abs(delta_times))
     wmatch = np.where(delta_times ==np.nanmin(delta_times))[0][0]
     delta_min = (guide_timestamps-obs_end_time)[wmatch]
 
     if verbose:
         print("Based on science DATE-END keyword and guiding timestamps, the matching GS file is: ")
-        print("   ", products[wmatch])  
+        print("   ", products[wmatch])
         print(f"    t_end = {obs_end_time}\t delta = {delta_min}")
-        
+
     if is_segmented[wmatch]:
         # We ought to fetch all the segmented GS files for that guide period
         products_to_fetch = [fn for fn in products if fn.startswith(products[wmatch][0:33])]
@@ -146,15 +146,15 @@ def find_relevant_guiding_file(sci_filename, verbose=True):
             print("   ".join(products_to_fetch))
     else:
         products_to_fetch = [products[wmatch],]
-    
+
     outfiles = mast_retrieve_guiding_files(products_to_fetch)
-    
+
     return outfiles
 
 def guiding_performance_plot(sci_filename, verbose=True, save=False):
     """Generate a plot showing the guiding jitter during an exposure
-    
-    
+
+
     """
 
     # Retrieve the guiding packet file from MAST
@@ -184,7 +184,7 @@ def guiding_performance_plot(sci_filename, verbose=True, save=False):
             pointing_table = astropy.table.vstack([pointing_table, pointing_table_more], metadata_conflicts='silent')
             centroid_table = astropy.table.vstack([centroid_table, centroid_table_more], metadata_conflicts='silent')
             display_gs_fn = os.path.basename(gs_fn)[0:33]+ "-seg*_cal.fits"
- 
+
 
     mask = centroid_table.columns['bad_centroid_dq_flag'] == 'GOOD'
 
@@ -195,24 +195,33 @@ def guiding_performance_plot(sci_filename, verbose=True, save=False):
     xmean = centroid_table[mask]['guide_star_position_x'].mean()
     ymean = centroid_table[mask]['guide_star_position_y'].mean()
 
-    
+
     # Create Plots
     fig, axes = plt.subplots(figsize=(16,12), nrows=3)
-    
+
     min_time = np.min([ptimes.plot_date.min(), ctimes.plot_date.min()])
     max_time = np.max([ptimes.plot_date.max(), ctimes.plot_date.max()])
     dtime = max_time - min_time
-    
+
+    # Find the subset of centroid data from during that exposure
+    ptimes_during_exposure = (t_beg < ptimes ) & (ptimes < t_end)
+    ctimes_during_exposure = (t_beg < ctimes[mask] ) & (ctimes[mask] < t_end)
+
+
     for i in range(3):
         axes[i].xaxis.axis_date()
         axes[i].set_xlim(min_time -0.01*dtime, max_time+0.01*dtime)
 
-    axes[0].set_title(f"Guiding during {os.path.basename(sci_filename)}", fontweight='bold', fontsize=18)    
-        
+    axes[0].set_title(f"Guiding during {os.path.basename(sci_filename)}", fontweight='bold', fontsize=18)
+
     axes[0].semilogy(ptimes.plot_date, pointing_table.columns['jitter'], alpha=1, color='C0')
     axes[0].set_ylim(1e-2, 1e3)
     axes[0].set_ylabel("Jitter\n[mas]", fontsize=18)
     axes[0].text(0.01, 0.95, display_gs_fn, fontsize=16, transform=axes[0].transAxes, verticalalignment='top')
+    axes[0].text(t_beg.plot_date, 20,
+                 f"    mean jitter: {pointing_table.columns['jitter'][ptimes_during_exposure].mean():.2f} mas", color='green')
+
+
 
     axes[0].axvspan(t_beg.plot_date, t_end.plot_date, color='green', alpha=0.15)
     axes[0].text(t_beg.plot_date, 50, " Exposure", color='green')
@@ -239,7 +248,126 @@ def guiding_performance_plot(sci_filename, verbose=True, save=False):
 
     outname = f'guidingplot_{gs_fn_base}.pdf'
 
-    if save: 
+    if save:
+        plt.savefig(outname)
+        if verbose:
+            print(f' ==> {outname}')
+
+
+
+def guiding_performance_jitterball(sci_filename, fov_size = 8, nbins=50, verbose=True, save=False):
+    """Generate a plot showing the guiding jitter during an exposure
+
+
+    """
+
+    # Retrieve the guiding packet file from MAST
+    gs_fns = find_relevant_guiding_file(sci_filename)
+
+    gs_fn = gs_fns[0]
+
+    # Determine start and end times for the exposure
+    with fits.open(sci_filename) as sci_hdul:
+        t_beg = astropy.time.Time(sci_hdul[0].header['DATE-BEG'])
+        t_end = astropy.time.Time(sci_hdul[0].header['DATE-END'])
+
+    # We may have multiple GS filenames returned, in the case where the data is split into several segments
+    # If so, concatenat them
+
+    for i, gs_fn in enumerate(gs_fns):
+        gs_fn_base = os.path.splitext(os.path.basename(gs_fn))[0]
+
+        if i==0:  # For a single file or first segment, just read it in
+            # Read the data from that file, and parse into astropy Times
+            pointing_table = astropy.table.Table.read(gs_fn, hdu=4)
+            centroid_table = astropy.table.Table.read(gs_fn, hdu=5)
+            display_gs_fn = os.path.basename(gs_fn)
+        else:  # for a later segment, read in and append
+            pointing_table_more = astropy.table.Table.read(gs_fn, hdu=4)
+            centroid_table_more = astropy.table.Table.read(gs_fn, hdu=5)
+            pointing_table = astropy.table.vstack([pointing_table, pointing_table_more], metadata_conflicts='silent')
+            centroid_table = astropy.table.vstack([centroid_table, centroid_table_more], metadata_conflicts='silent')
+            display_gs_fn = os.path.basename(gs_fn)[0:33]+ "-seg*_cal.fits"
+
+
+    mask = centroid_table.columns['bad_centroid_dq_flag'] == 'GOOD'
+
+    ctimes = astropy.time.Time(centroid_table['observatory_time'])
+    ptimes = astropy.time.Time(pointing_table.columns['time'], format='mjd')
+
+    # Compute the mean X and Y positions
+    xmean = centroid_table[mask]['guide_star_position_x'].mean()
+    ymean = centroid_table[mask]['guide_star_position_y'].mean()
+
+
+    # Create Plots
+    fig= plt.figure(figsize=(8,8),)
+
+
+    # Add a gridspec with two rows and two columns and a ratio of 1 to 4 between
+    # the size of the marginal axes and the main axes in both directions.
+    # Also adjust the subplot parameters for a square plot.
+    gs = fig.add_gridspec(2, 2,  width_ratios=(4, 1), height_ratios=(1, 4),
+                          left=0.1, right=0.9, bottom=0.1, top=0.9,
+                          wspace=0.05, hspace=0.05)
+    # Create the Axes.
+    ax = fig.add_subplot(gs[1, 0])
+    ax_histx = fig.add_subplot(gs[0, 0], sharex=ax)
+    ax_histy = fig.add_subplot(gs[1, 1], sharey=ax)
+    # Draw the scatter plot and marginals.
+    #scatter_hist(x, y, ax, ax_histx, ax_histy)
+
+
+
+    min_time = np.min([ptimes.plot_date.min(), ctimes.plot_date.min()])
+    max_time = np.max([ptimes.plot_date.max(), ctimes.plot_date.max()])
+    dtime = max_time - min_time
+
+    # Find the subset of centroid data from during that exposure
+    ptimes_during_exposure = (t_beg < ptimes ) & (ptimes < t_end)
+    ctimes_during_exposure = (t_beg < ctimes[mask] ) & (ctimes[mask] < t_end)
+
+    xpos = centroid_table[mask][ctimes_during_exposure]['guide_star_position_x']
+    ypos = centroid_table[mask][ctimes_during_exposure]['guide_star_position_y']
+
+    xmean = xpos.mean()
+    ymean = ypos.mean()
+
+    rpos = np.sqrt(xpos**2+ypos**2)
+    print(np.std(rpos))
+
+    xoffsets = (xpos-xmean)*1000
+    yoffsets = (ypos-ymean)*1000
+
+    ax.scatter(xoffsets, yoffsets, alpha = 0.8, marker='+')
+    ax.set_aspect('equal')
+
+    ax.set_xlim(-fov_size/2, fov_size/2)
+    ax.set_ylim(-fov_size/2, fov_size/2)
+    ax.set_xlabel("GS Centroid Offset X [milliarcsec]", fontsize=18)
+    ax.set_ylabel("GS Centroid Offset Y [milliarcsec]", fontsize=18)
+
+
+    fig.suptitle(f"Guiding during {os.path.basename(sci_filename)}\n", fontweight='bold', fontsize=18)
+
+
+    for rad in [1,2,3]:
+        ax.add_artist(plt.Circle( (0,0), rad, fill=False, color='gray', ls='--'))
+        if rad<fov_size/2:
+            ax.text(0, rad+0.1, f"{rad} mas", color='gray')
+
+
+    # Draw histograms
+    ax_histx.tick_params(axis="x", labelbottom=False)
+    ax_histy.tick_params(axis="y", labelleft=False)
+
+    bins = np.linspace(-fov_size/2, fov_size/2, nbins)
+    ax_histx.hist(xoffsets, bins=bins)
+    ax_histy.hist(yoffsets, bins=bins, orientation='horizontal')
+
+    outname = f'guidingball_{gs_fn_base}.pdf'
+
+    if save:
         plt.savefig(outname)
         if verbose:
             print(f' ==> {outname}')
